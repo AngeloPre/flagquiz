663074deacb610321ae1dbc93b28c2cf33c536a6
Mr-RenatoBlue <renatomedeirosrocha@gmail.com>
2026-03-30 19:44:21 -0300

    bandeiras + init quis view

--- app/src/main/java/com/example/flagquiz/controller/QuizActivity.kt
+++ app/src/main/java/com/example/flagquiz/controller/QuizActivity.kt
@@ -1,6 +1,7 @@
 package com.example.flagquiz.controller
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,14 +9,19 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.flagquiz.R
 
 class QuizActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_quiz)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val ivFlag = findViewById<ImageView>(R.id.ivFlag)
+        ivFlag.setImageResource(R.drawable.flag_br)
     }
 }
